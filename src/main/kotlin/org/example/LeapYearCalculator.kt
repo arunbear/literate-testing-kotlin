53cb39e61c5931edb8aaafb2469bca854f16a0db
arunp <arunbear@cpan.org>
2025-04-14 11:09:12 +0100

    Fix divisibility by 400

--- src/main/kotlin/org/example/LeapYearCalculator.kt
+++ src/main/kotlin/org/example/LeapYearCalculator.kt
@@ -1,5 +1,9 @@
 package org.example
 
 fun isALeapYear(year: Int): Boolean {
-    return year % 4 == 0 && year % 100 != 0
+    return when {
+        year % 4 == 0 && year % 100 != 0 -> true
+        year % 400 == 0 -> true
+        else -> false
+    }
 }

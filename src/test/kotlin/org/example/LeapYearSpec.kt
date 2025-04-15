a9b8bb9778dc584ce33568615d394d6a4cebc154
arunp <arunbear@cpan.org>
2025-04-15 17:34:11 +0100

    Use BDD assertions to make test names more readable

--- src/test/kotlin/org/example/LeapYearSpec.kt
+++ src/test/kotlin/org/example/LeapYearSpec.kt
@@ -1,7 +1,6 @@
 package org.example
 
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.BDDAssertions.*
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores
 import org.junit.jupiter.api.IndicativeSentencesGeneration
 import org.junit.jupiter.api.Nested
@@ -18,13 +17,13 @@ class LeapYearSpec {
         @ParameterizedTest
         @ValueSource(ints = [2004, 1984, 4])
         fun `if it is divisible by 4 but not by 100`(year: Int) {
-            assertThat(isALeapYear(year)).isTrue()
+            then(isALeapYear(year)).isTrue()
         }
 
         @ParameterizedTest
         @ValueSource(ints = [2000, 1600, 400])
         fun `if it is divisible by 400`(year: Int) {
-            assertThat(isALeapYear(year)).isTrue()
+            then(isALeapYear(year)).isTrue()
         }
 
     }
@@ -36,13 +35,13 @@ class LeapYearSpec {
         @ParameterizedTest
         @ValueSource(ints = [2022, 2019, 1999, 1])
         fun `if it is not divisible by 4`(year: Int) {
-            assertThat(isALeapYear(year)).isFalse()
+            then(isALeapYear(year)).isFalse()
         }
 
         @ParameterizedTest
         @ValueSource(ints = [2100, 1900, 100])
         fun `if it is divisible by 100 but not by 400`(year: Int) {
-            assertThat(isALeapYear(year)).isFalse()
+            then(isALeapYear(year)).isFalse()
         }
     }
 
@@ -52,8 +51,7 @@ class LeapYearSpec {
         @ParameterizedTest
         @ValueSource(ints = [1, Int.MAX_VALUE])
         fun `if it is positive`(year: Int) {
-            Assertions.assertThatNoException()
-                .isThrownBy { isALeapYear(year) }
+            thenNoException().isThrownBy { isALeapYear(year) }
         }
     }
 
@@ -62,15 +60,13 @@ class LeapYearSpec {
     inner class A_year_is_not_supported {
         @Test
         fun `if it is zero`() {
-            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
-                .isThrownBy { isALeapYear(0) }
+            thenExceptionOfType(IllegalArgumentException::class.java).isThrownBy { isALeapYear(0) }
         }
 
         @ParameterizedTest
         @ValueSource(ints = [-1, -4, -100, -400, Int.MIN_VALUE])
         fun `if it is negative`(year: Int) {
-            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
-                .isThrownBy { isALeapYear(year) }
+            thenExceptionOfType(IllegalArgumentException::class.java).isThrownBy { isALeapYear(year) }
         }
     }
 
